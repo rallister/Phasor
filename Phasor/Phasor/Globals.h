3857549a61460ca7d79793b1263c0ab8e5ede170
kami <xa@xa.xa>
2014-01-02 01:27:11 +0000

    missed.

--- Phasor/Phasor/Globals.h
+++ Phasor/Phasor/Globals.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Logging.h"
+#include "GameLogging.h"
 #include "PhasorThread.h"
 #include "Halo/HaloStreams.h"
 #include "../Common/Timers.h"
@@ -7,7 +9,11 @@
 // defined in main.cpp
 extern PhasorThread g_Thread;
 extern Timers g_Timers; 
-
+extern std::unique_ptr<CGameLog> g_GameLog;
+extern std::unique_ptr<CScriptsLog> g_ScriptsLog;
+extern std::unique_ptr<CPhasorLog> g_PhasorLog;
+extern std::unique_ptr<CRconLog> g_RconLog;
+extern std::unique_ptr<halo::CHaloPrintStream> g_PrintStream;
 
 extern void _DO_TRACE(const char* fmt...);
 extern FILE * _TRACE_FILE;
